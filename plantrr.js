263289d7bcbcb127e9982d8c2b184246f8f41858
Henry <mcintyrehh@gmail.com>
2019-03-22 14:44:09 -0400

    adding start script

--- plantrr.js
+++ plantrr.js
@@ -1,8 +1,18 @@
 Module.register("plantrr",{
+	result: [],
 	defaults: {
-		text: "Hello World!"
+		text: "Henry Test Module!"
 	},
 	//override dom generator
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// Schedule update interval.
+		var self = this;
+		setInterval(function() {
+			self.updateDom();
+		}, 1000);
+	},
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = this.config.text;
