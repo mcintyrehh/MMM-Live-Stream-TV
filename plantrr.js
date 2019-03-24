47b6ebe829e29421b00d5722cd65f36941bff590
mcintyrehh <mcintyrehh@gmail.com>
2019-03-23 20:53:06 -0400

    test module is populating now

--- plantrr.js
+++ plantrr.js
@@ -1,22 +1,9 @@
 Module.register("plantrr",{
-	result: [],
-	defaults: {
-		text: "Henry Test Module!"
-	},
-	//override dom generator
-	start: function() {
-		Log.info("Starting module: " + this.name);
-
-		// Schedule update interval.
-		var self = this;
-		setInterval(function() {
-			self.updateDom();
-		}, 1000);
-	},
 	getDom: function() {
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
-		return wrapper;
-	}
-
-});
+			var wrapper = document.createElement("testDiv");
+			wrapper.className = 'test';
+			wrapper.innerHTML = "Henry test test testaroonski";
+			return wrapper;
+		}
+	
+	});
