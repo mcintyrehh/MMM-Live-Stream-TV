f2ef9e3c345f063676772f1a8e9222e77520fb22
Henry <mcintyrehh@gmail.com>
2019-03-24 20:42:09 -0400

    added 10 live streams of nyc

--- plantrr.js
+++ plantrr.js
@@ -1,9 +1,52 @@
 Module.register("plantrr",{
 	defaults: {
-		frameWidth: "300",
+		frameWidth: "600",
 		maxWidth: "100%",
-		url: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-		scrolling: "no"
+		// url: "http://iphone-streaming.ustream.tv/uhls/1524/streams/live/iphone/playlist.m3u8",
+		url: "https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w2084964165.m3u8",
+		scrolling: "no",
+		streams: [
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w2084964165.m3u8",
+				name: "times-square"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/13908.flv/chunklist_w1570249403.m3u8",
+				name: "empire-state-building"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/13900.flv/chunklist_w1596516954.m3u8",
+				name: "empire-state-building-south"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/10874.flv/chunklist_w1710102470.m3u8",
+				name: "world-trade-center"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/chargingbull.flv/chunklist_w924600150.m3u8",
+				name: "wall-street-bull"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/4017timessquare.flv/chunklist_w2099074996.m3u8",
+				name: "times-square-south"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/485.flv/chunklist_w1369316787.m3u8",
+				name: "times-square-north"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w212315235.m3u8",
+				name: "times-square-street"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/15559.flv/chunklist_w1260148854.m3u8",
+				name: "times-square-crossroads"
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/7384.flv/chunklist_w1375826723.m3u8",
+				name: "high-line"
+			}
+		]
 	},
 	start: function() {
 		console.log("in plantrr");
@@ -18,9 +61,10 @@ Module.register("plantrr",{
 	getDom: function() {
 		var { width, height } = this.config;
 		var video = document.createElement("video");
+		video.width = this.config.frameWidth;
 		if (Hls.isSupported()) {
 			var hls = new Hls();
-			hls.loadSource("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
+			hls.loadSource(this.config.streams[6].url);
 			hls.attachMedia(video);
 			hls.on(Hls.Events.MANIFEST_PARSED,function() {
 				video.play();
@@ -35,5 +79,4 @@ Module.register("plantrr",{
 		video.play();
 		return video;
 	}
-
 });
