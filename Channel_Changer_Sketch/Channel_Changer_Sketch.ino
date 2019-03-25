dae290b161fd71ea054d8e36635b705af74520d5
mcintyrehh <mcintyrehh@gmail.com>
2019-03-24 22:26:59 -0400

    raspmagic mirror now receiving data from arduino

--- Channel_Changer_Sketch/Channel_Changer_Sketch.ino
+++ Channel_Changer_Sketch/Channel_Changer_Sketch.ino
@@ -1,10 +1,28 @@
 int adcValue;
 int ledPin = 9;
+volatile int32_t m_counter = 0;
+bool WasStarted = false;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);       // initialize the serial port and set baud rate to 9600
-  Serial.println("UNO is ready!");
+  while(!Serial);
+
+  Serial.println("[status:setup:starting]");
+  
+  int test = 1;
+  m_counter = test;
+
+  if (test > 1) {
+    WasStarted = false;
+    Serial.println("[status:setup:failed]");
+    return;    
+  }
+
+  delay(3000);
+  WasStarted = true;
+  Serial.println("[status:setup:started]");
+  delay(100);
 }
 
 void loop() {
@@ -12,7 +30,9 @@ void loop() {
   //map analog to the 0-255 range, and works as PWM duty cycle of ledPin port
   analogWrite(ledPin, map(adcValue, 0, 1023, 0, 255));
   // Send the result to computer through serial
-  Serial.print("[sensor:Potentiometer:");
-  Serial.println(map(adcValue, 0, 1023, 0, 10));
-  delay(500);
+  Serial.print("[sensor:PTNM:");
+  Serial.print(map(adcValue, 0, 1023, 0, 10));
+  Serial.println("]");
+  
+  delay(1000);
 }
