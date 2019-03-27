3cbd3749546ffddf90f5c057b5374084d74eb299
Henry <mcintyrehh@gmail.com>
2019-03-27 10:04:50 -0400

    consolidating dependencies from MMM-ArduPort

---
+++ MMM-Live-Stream-TV.js
@@ -0,0 +1,245 @@
+Module.register("MMM-Live-Stream-TV",{
+	logPrefix: "[MMM-Live-Stream-TV]:: ",
+
+	defaults: {
+		portname: "COM7",
+		updateInterval: 1, //second
+		animationSpeed: 1000,
+		displayIcons: true,
+		iconSize: "small",
+		labelSize: "medium",
+		showDescription: false,
+		developerMode: false,
+		frameWidth: "600",
+		maxWidth: "100%",
+		url: "https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w2084964165.m3u8",
+		scrolling: "no",
+		currentChannel: 0,
+		streams: [
+
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/13908.flv/chunklist_w1570249403.m3u8",
+				name: "Empire State Building",
+				channelNumber: 0,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/13900.flv/chunklist_w1596516954.m3u8",
+				name: "ESB View (South)",
+				channelNumber: 1,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/10874.flv/chunklist_w1710102470.m3u8",
+				name: "World Trade Center",
+				channelNumber: 2,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/chargingbull.flv/chunklist_w924600150.m3u8",
+				name: "Wall Street Bull",
+				channelNumber: 3,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/hdtimes10.flv/chunklist_w1219632180.m3u8",
+				name: "Times Square",
+				channelNumber: 4,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/4017timessquare.flv/chunklist_w2099074996.m3u8",
+				name: "Times Square View (South)",
+				channelNumber: 5,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/485.flv/chunklist_w1369316787.m3u8",
+				name: "Times Square View (North)",
+				channelNumber: 6,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w212315235.m3u8",
+				name: "Times Square Street Cam",
+				channelNumber: 7,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/15559.flv/chunklist_w1260148854.m3u8",
+				name: "Times Square Crossroads",
+				channelNumber: 8,
+			},
+			{
+				url: "https://videos3.earthcam.com/fecnetwork/13903.flv/chunklist_w980148749.m3u8",
+				name: "Midtown Skyline",
+				channelNumber: 9,
+			}
+		]
+	},
+	start: function() {
+		console.log("in plantrr");
+		Log.info(`Starting module: ${this.name}`);
+		const self = this;
+		// initializing hls variable to be referenced when creating/destroying streams
+		this.hls = "";
+		// initializing status booleans
+		this.loading = true;
+		this.isArduinoStarting = false;
+		this.isArduinoStarted = false;
+		this.isArduinoConnected = false;
+		// sends the configuration details to node_helper
+		console.log(this.config);
+		self.log(this.config);
+		this.sendSocketNotification("CONFIG", this.config);
+		// sends a notification to node_helper to intialize the python shell
+		this.sendSocketNotification("INITIALIZE", null);
+
+		self.log(("[MMM-Live-Stream-TV::START()]: data: " + JSON.stringify(self.data, null, 4)), "dev");
+		self.log(("[MMM-Live-Stream-TV::START()]: config: " + JSON.stringify(self.config, null, 4)), "dev");
+	},
+	getTranslations: function() {
+		return {
+			en: "translations/en.json"
+		};
+	},
+	getStyles: function() {
+		return [
+			"https://use.fontawesome.com/releases/v5.6.3/css/all.css",
+			"MMM-Live-Stream-TV.css"
+		];
+	},
+	// Define required scripts.
+	getScripts: function() {
+		// this loads the hls js file
+		return ["https://cdn.jsdelivr.net/npm/hls.js"];
+	},
+	socketNotificationReceived: function(notification, payload) {
+		const self = this;
+
+		self.log(notification);
+		self.log(JSON.stringify(payload, null, 4));
+
+		switch(notification){
+		case "status":
+			if(payload.name == "setup"){
+				if(payload.data == "starting"){
+					self.log("[socketNotificationReceived::status]: starting");
+					this.isArduinoConnected = true;
+					this.isArduinoStarting = true;
+				} else if (payload.data == "started"){
+					self.log("[socketNotificationReceived::status]: started");
+					this.isArduinoStarting = false;
+					this.isArduinoStarted = true;
+				} else if (payload.data == "failed"){
+					self.log("[socketNotificationReceived::status]: failed");
+					this.isArduinoStarting = false;
+					this.isArduinoStarted = false;
+				}
+			}
+			if(payload.name == "connect"){
+				if(payload.data == "connected"){
+					self.log("[socketNotificationReceived::status]: connected");
+					this.isArduinoConnected = true;
+					this.sendNotification("SHOW_ALERT", {type: "notification", imageFA: "fa-microchip", title: self.translate("ARDUINO_CONNECTED")});
+				} else if(payload.data == "disconnected"){
+					self.log("[socketNotificationReceived::status]: disconnected");
+					this.isArduinoConnected = false;
+					this.sendNotification("SHOW_ALERT", {type: "notification", imageFA: "fa-microchip", title: self.translate("ARDUINO_DISCONNECTED")});
+				}
+			}
+			if(payload.name == "initialized"){
+				this.loading = false;
+			}
+			self.updateDom(self.config.animationSpeed);
+			break;
+
+		case "sensor":
+			if(this.isArduinoConnected && this.isArduinoStarted){
+				self.log("[socketNotificationReceived::sensor]: ");
+				self.log(JSON.stringify(payload, null, 4));
+
+				this.message = null;
+				this.config.sensors = payload;
+
+				self.updateDom(self.config.animationSpeed);
+			}
+			break;
+
+		case "error":
+			console.log("[socketNotificationReceived::error]:");
+
+			if(payload == "pyshell-throw"){
+				this.message = "Error : PyShell down!";
+				this.isArduinoConnected = false;
+				this.isArduinoStarted = false;
+			}
+
+			self.updateDom(self.config.animationSpeed);
+			break;
+		}
+	},
+	// Received notifications from ArduPort, which sends Arduino Serial value
+	notificationReceived: function(notification, payload, sender) {
+		// if it has a sender it isn't a system notification
+		if (sender) {
+			Log.log(`${this.name} received a module notification: ${notification} from sender: ${sender.name}`);
+			if (newChannel != this.config.currentChannel) {
+				var newChannel = parseInt(payload[0].value);
+				Log.log(newChannel);
+				// sets current channel to the value that the Arduino Serial is sending out
+				this.config.currentChannel = newChannel;
+				this.destroyHLS();
+			} else {
+				Log.log("not loaded yet")
+			};
+		} else {
+			Log.log(`${this.name} received a system notification: ${notification}`)
+		}
+	},
+	destroyHLS: function() {
+		this.hls.destroy();
+		this.updateDom();
+	},
+	getDom: function(destroy) {
+		var wrapper = document.createElement("div");
+		var { width, height } = this.config;
+		var video = document.createElement("video");
+		wrapper.appendChild(video);
+		// creating info divs below the video
+		var videoInfo = document.createElement("div");
+		var channelNumber = document.createElement("span");
+		channelNumber.className=("channel-div");
+		channelNumber.innerHTML = `Channel ${this.config.currentChannel}`;
+		videoInfo.appendChild(channelNumber);
+		var videoName = document.createElement("span");
+		videoName.innerHTML = ` - ${this.config.streams[this.config.currentChannel].name}`
+		videoInfo.appendChild(videoName);
+		wrapper.appendChild(videoInfo);
+		video.width = this.config.frameWidth;
+		if (Hls.isSupported()) {
+			var hls = new Hls();
+			this.hls = hls;
+			hls.loadSource(this.config.streams[this.config.currentChannel].url);
+			hls.attachMedia(video);
+			hls.on(Hls.Events.MANIFEST_PARSED,function() {
+				video.play();
+			})
+		}
+		else if(video.canPlayType("application/vnd.apple.mpegurl")) {
+			video.src = this.config.url;
+			video.addEventListener("loadedmetadata", function() {
+				video.play();
+			});
+		}
+		video.play();
+		return wrapper;
+	},
+	log: function(message, type) {
+		var self = this;
+		if (self.config.developerMode) {
+			  var date = new Date();
+			  var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+			  message = self.name + ": (" + self.data.index + ")(" + time + ") " + message;
+		} else { message = self.name + ": " + message; }
+		switch (type) {
+		  case "error": Log.error(this.logPrefix + message); break;
+		  case "warn": Log.warn(this.logPrefix + message); break;
+		  case "info": Log.info(this.logPrefix + message); break;
+		  case "dev": if (self.config.developerMode) { Log.log(this.logPrefix + message); } break;
+		  default: Log.log(this.logPrefix + message);
+		}
+	}
+});
---
+++ arduport/arduport.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# coding: utf8
+
+# ====================================================
+# MMM-ArduPort Copyright(C) 2019 Furkan Türkal
+# // This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+# and you are welcome to redistribute it under certain conditions; See
+# file LICENSE, which is part of this source code package, for details.
+# ====================================================
+
+import os
+import sys
+import time
+import stat
+import json
+import threading
+import datetime
+import glob
+import serial
+
+import arduino
+
+
+def to_node(type, message):
+    try:
+        print(json.dumps({type: message}))
+    except Exception:
+        pass
+
+    sys.stdout.flush()
+
+
+# https://stackoverflow.com/a/14224477/5685796
+def get_serial_ports():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+def start_scanner():
+    con_list = {}
+
+    while True:
+        # Scan all ports
+        ports = get_serial_ports()
+
+        # If a device connected
+        if len(ports) > 0:
+
+            # For-Loop in the scanned ports
+            for current in ports:
+
+                # New Arduino is connected
+                # Register to con_list
+                # Returns False because we don't connect yet
+                if current not in con_list:
+                    con_list[current] = False
+
+                # Arduino is connected
+                if current in con_list and con_list[current] == False:
+                    con = arduino.Arduino(current, 9600)
+                    if con.open(max_attempt = 5):
+                        con_list[current] = True
+                        to_node("status", {"name": "connect", "data": "connected"})
+
+                        thread = threading.Thread(target = con.start_serial())
+                        thread.start()
+
+                # Arduino in con_list, nothing to do
+                elif current in con_list and con_list[current] == True:
+                    continue
+
+
+        # Any Arduino connected, set them all False
+        # We are not goint to clean the list because we will check if it is connected again
+        elif len(con_list) > 0:
+            for con in con_list:
+                if con_list[con] == True:
+                    con_list[con] = False
+                    to_node("status", {"name": "connect", "data": "disconnected"})
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    to_node("debug", 'SerialPort shell started...')
+    to_node("debug", 'Waiting Ardunio to connect on port...')
+
+    scanner = threading.Thread(target = start_scanner)
+    scanner.start()
